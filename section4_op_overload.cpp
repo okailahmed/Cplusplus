626eab45b8e7d716f5c8e6b4be2c9bd7100d744f
okailahmed <ahmedkareem808@gmail.com>
2022-11-28 16:17:46 +0200

    working with template in this commit

--- section4_op_overload.cpp
+++ section4_op_overload.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 
 
+
+template<class T>
+
+class B{
+    public:
+        T x;
+};
+
 class MyClass{
     int n;
     public:
@@ -23,14 +31,25 @@ class MyClass{
         }
 };
 
+
+
+
 int main(){
 
-        MyClass m1 (4);
-        MyClass m2 (2);
-        MyClass m3 (0);
+        // MyClass m1 (4);
+        // MyClass m2 (2);
+        // MyClass m3 (0);
 
-        m3 = m1+m2;
-        m3.print();
+        // m3 = m1+m2;
+        // m3.print();
+
+        B<int> b1;  // this mean i create new object with int date type 
+        b1.x = 10;
+
+        B<string> b2;
+        b2.x="Ahmed";      
+        
+          
 
     return 0;
 }
